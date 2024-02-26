6ba6be3ca3af4151c4e41ccb30c571f1707ddc42
Filip Jirsák <filip@jirsak.org>
2024-02-26 20:39:22 +0100

    Přejmenování metod – i v zakomentované části kódu.

--- src/main/java/cz/czechitas/lekce7/HlavniProgram.java
+++ src/main/java/cz/czechitas/lekce7/HlavniProgram.java
@@ -1,5 +1,8 @@
 package cz.czechitas.lekce7;
 
+import java.time.Month;
+import java.time.MonthDay;
+
 /**
  * Hlaví třída pro lekci 7.
  */
@@ -17,17 +20,17 @@ public class HlavniProgram {
         System.out.printf("Monika má svátek %s.", svatky.vratKdyMaSvatek("Monika")).println();
         System.out.printf("Eva je v seznamu svátků: %b.", svatky.jeVSeznamu("Eva")).println();
 
-/*
         System.out.println("V seznamu jsou následující jména:");
         svatky.getSeznamJmen().forEach(jmeno -> System.out.printf("- %s", jmeno).println());
 
-        svatky.smazatSvatek("Přemysl");
+/*
+        svatky.smazSvatek("Přemysl");
         System.out.printf("Po smazání Přemysla je v seznamu je %d jmen.", svatky.getPocetJmen()).println();
 
-        svatky.pridatSvatek("Laura", 1, 6);
-        svatky.pridatSvatek("Jarmil", 2, Month.JUNE);
-        svatky.pridatSvatek("Tamara", MonthDay.of(6, 3));
-        svatky.pridatSvatek("Kevin", MonthDay.of(Month.JUNE, 3));
+        svatky.pridejSvatek("Laura", 1, 6);
+        svatky.pridejSvatek("Jarmil", 2, Month.JUNE);
+        svatky.pridejSvatek("Tamara", MonthDay.of(6, 3));
+        svatky.pridejSvatek("Kevin", MonthDay.of(Month.JUNE, 3));
         System.out.printf("Po přidání Laury, Jarmila, Tamary a Kevina je v seznamu je %d jmen.", svatky.getPocetJmen()).println();
 */
     }
